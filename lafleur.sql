c8ca3ff3b0adcadb511f5023a6dd57de4b493b75
ugobat <80512752+syfo0@users.noreply.github.com>
2023-04-17 16:09:53 +0200

    affichage des produits
    + correction de certain produit voire lafleur.sql

--- lafleur.sql
+++ lafleur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 17 avr. 2023 à 12:47
+-- Généré le : lun. 17 avr. 2023 à 14:08
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -135,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `t_produit` (
 INSERT INTO `t_produit` (`idProduit`, `Designation`, `prix`, `Stock`, `Description`, `Image`, `idCategorie`) VALUES
 (1, 'La rose grimpant Pierre de Ronsard', 9.99, 155, 'C\'est un rosier grimpant à grandes fleurs très doubles ayant la forme des roses anciennes, globuleuses, d\'une couleur formant un dégradé du rose carminé central au rose délicat de l\'extérieur.', 'roseGrimpantPierreDeRonsard.png', 1),
 (2, 'hortensia', 19.99, 586, 'L’Hortensia est un arbuste à fleurs incontournable qui nous ravit au jardin de sa longue floraison, de juin à septembre ou octobre.', 'hortensia.png', 2),
-(3, 'le Cœur de Marie', 29.99, 5786, 'Le Cœur de Marie est une plante vivace, qui a des fleurs roses et blanches en forme de cœur. Elle peut atteindre 120 cm de haut et 45 cm de large.', 'coeurDeMarie.png', 3),
+(3, 'le Coeur de Marie', 29.99, 5786, 'Le Coeur de Marie est une plante vivace, qui a des fleurs roses et blanches en forme de cœur. Elle peut atteindre 120 cm de haut et 45 cm de large.', 'coeurDeMarie.png', 3),
 (4, 'Tulipe triomphe \'Jan Reus\'', 14.99, 369, 'La Tulipe Triomphe \'Jan Reus\' est recherchée pour son élégance et sa vivacité. Cette tulipe majestueuse à corolle étroite offre des pétales rouge bordeaux velouté s’associant à merveille avec des coloris lilas et orangé.', 'tulipeJanReus.png', 3),
 (5, 'Tulipe Triomphe \'Barcelona\'', 16.99, 153, 'La Tulipe Triomphe Barcelona est intéressante pour son coloris très tendance.', 'tulipeBarcelona.png', 3),
 (6, 'Tulipe simple tardive \'Reine de la Nuit\'', 9.99, 200, 'La Tulipe Simple tardive \'Reine de la Nuit\' est sans doute la meilleure et la plus sombre des tulipes dites \'noires\'', 'tulipeReineDeLaNuit.png', 3),
@@ -147,8 +147,8 @@ INSERT INTO `t_produit` (`idProduit`, `Designation`, `prix`, `Stock`, `Descripti
 (12, 'Fleur de sakura', 40.99, 25, 'La fleur de sakura est la fleur du cerisier ornemental du Japon, qui appartient au genre Prunus. Elle est de couleur rose ou blanche et a cinq pétales.', 'sakura.png', 4),
 (13, 'Le narcisse', 9.99, 436, 'Le narcisse est une plante bulbeuse qui appartient à la famille des amaryllidacées. Il existe de nombreuses espèces et variétés de narcisse, qui se distinguent par la couleur et la forme de leurs fleurs.', 'narcisse.png', 3),
 (14, 'Heliconia', 34.99, 87, 'L’Heliconia est une plante à fleur qui appartient à la famille des Heliconiacées, qui ne comprend qu’un seul genre.', 'heliconia.png', 2),
-(15, 'L’Hibiscus', 60.99, 96, 'Les fleurs sont généralement grandes et colorées, avec une forme de trompette et cinq pétales. Elles peuvent être roses, rouges, oranges, blanches ou jaunes.', 'hibiscus.png', 3),
-(16, 'Anémone', 12.99, 79, 'L’anémone de terre est une plante à fleur qui appartient à la famille des Renonculacées. Elle se présente sous forme de bulbe ou de rhizome et produit des fleurs simples ou doubles, souvent rouges, bleues ou blanches.', 'anémone.png', 3),
+(15, 'L\'Hibiscus', 60.99, 96, 'Les fleurs sont généralement grandes et colorées, avec une forme de trompette et cinq pétales. Elles peuvent être roses, rouges, oranges, blanches ou jaunes.', 'hibiscus.png', 3),
+(16, 'Anémone', 12.99, 79, 'L’anémone de terre est une plante à fleur qui appartient à la famille des Renonculacées. Elle se présente sous forme de bulbe ou de rhizome et produit des fleurs simples ou doubles, souvent rouges, bleues ou blanches.', 'anemone.png', 3),
 (17, 'Jacinthe ', 25.99, 468, 'La jacinthe est une plante à bulbe qui appartient à la famille des Hyacinthacées. Elle est originaire de Méditerranée orientale. Elle produit des fleurs en forme de clochettes, simples ou doubles, aux couleurs variées et très parfumées.', 'jacinthe.png', 3),
 (18, 'Schlumbergera ', 30.99, 50, 'Le schlumbergera est un genre de cactus sans épines qui compte six à neuf espèces originaires des montagnes côtières du sud-est du Brésil.', 'schlumbergera.png', 5),
 (19, 'Abutilon ', 20.99, 487, 'L’abutilon est un grand genre de plantes à fleurs de la famille des Malvacées. Il est réparti dans les régions tropicales et subtropicales des Amériques, d’Afrique, d’Asie et d’Australie.', 'abutilon.png', 2),
