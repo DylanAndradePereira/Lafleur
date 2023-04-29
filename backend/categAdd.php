13f2038f90765d19596013a075ce720fb7d3c6e3
shiki futaki pp <100916708+ShikiFutakiPP@users.noreply.github.com>
2023-04-29 13:37:08 +0200

    Create categAdd.php

---
+++ backend/categAdd.php
@@ -0,0 +1,15 @@
+<?php
+
+include '../connection.php';
+
+//Envoi dans la base de données
+$stmt = $dbh->prepare("INSERT INTO t_categorie(`libelle`) VALUES (:libelle)");
+$stmt->bindParam(':libelle', $_REQUEST["libelle"]);
+$stmt->execute();
+
+header("location: BackOffice.php");   
+
+?>
+
+
+
