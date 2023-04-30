5e7993e21cdae42c5a5b7d8e340bf5a963fb1f8e
shiki futaki pp <100916708+ShikiFutakiPP@users.noreply.github.com>
2023-05-01 00:57:30 +0200

    Recherche dans la boutique

--- shop.php
+++ shop.php
@@ -1,7 +1,5 @@
 <?php include 'header.php'?>
-
     <main>
-
         <div id="navCategories">
             <ul>
             <!-- Boucle des catégories -->
@@ -9,22 +7,50 @@
                 require 'connection.php';
                 $table = $dbh->query('SELECT * FROM t_categorie');
                 $tableProduits = $table->fetchAll();
+                
+                if (isset($_GET["categ"]) and !empty($_GET["categ"])){
+                    $categ = $_GET["categ"];
+                }
                 foreach ($tableProduits as $ligne) {
-                    echo "<li>".$ligne['libelle']."</li>";
+                    echo "<a href='shop.php?categ=".$ligne['idCategorie']."' style='text-decoration: none;color: black'>
+                        <li";
+                        if (isset($categ) && ($categ==$ligne['idCategorie'])){
+                            echo ' style="border:solid black"';                
+                        }
+                        echo ">".$ligne['libelle']."</li></a>";
                 } 
             ?>
             </ul>
         </div>
 
-        <div class="rechercheShop">
-            <input type="text" name="" id="">
+        <form class="rechercheShop">
+            <input type="text" name="recherche" id="recherche" 
+                <?php
+                    if(isset($_GET["recherche"]) and !empty($_GET["recherche"])){
+                        $recherche= htmlspecialchars($_GET["recherche"]);
+                        echo 'value="'.$recherche.'"';
+                    }
+                ?>
+                style="width:100%;height:auto;padding-left:20px">
             <button type="submit"></button>
-        </div>
-
-        <div id="shop">
-            <!-- Boucle des articles -->
-            <?php
-                $table = $dbh->query('SELECT * FROM t_produit');
+        </form>
+        <!-- Boucle des articles -->
+        <?php
+            if(isset($recherche)){
+                //Articles selon une recherche
+                $table = $dbh->prepare('SELECT * FROM t_produit WHERE Designation LIKE CONCAT("%", :recherche, "%")');
+                $table->bindParam(':recherche', $recherche);
+                $table->execute();
+            } else {
+                if (isset($categ)){
+                    $table = $dbh->prepare('SELECT * FROM t_produit WHERE idCategorie=:categ');
+                    $table->bindParam(':categ', $_GET["categ"]);
+                    $table->execute();
+                } else {
+                    $table = $dbh->query('SELECT * FROM t_produit');
+                }
+            }
+            echo '<div id="shop">';
                 $tableProduits = $table->fetchAll();
                 foreach ($tableProduits as $ligne) {
                     echo '<div class="card">
@@ -35,7 +61,11 @@
                                 <button type="submit" id="commande_'.$ligne['idProduit'].'">COMMANDER</button>
                             </form>
                         </div>';
+
                 } 
+                if(($table -> rowCount()) == 0){ 
+                    echo "<div style='font-size: 2vw;'>Aucun résultat</div>";
+                }
             ?>         
         </div>
     </main>
