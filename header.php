2291d4b4ae6a1763d31a12f778aeaf28e9d32ceb
shiki futaki pp <100916708+ShikiFutakiPP@users.noreply.github.com>
2023-05-01 19:27:35 +0200

    retirer du panier

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="./css/index.css">
-    <title>Document</title>
+    <title>Lafleur</title>
 </head>
 <body> 
 
